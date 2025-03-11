3f1a4b357419dd36347f7857bd98e9b9b345cf63
20100 <mmmelkady23@gmail.com>
2025-03-11 23:18:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_Core_04_Join_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
